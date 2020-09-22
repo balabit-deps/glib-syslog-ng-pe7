2eedd7638d80d071a055f5886272ffd2a4d46a60
Attila Szakacs <attila.szakacs@oneidentity.com>
2020-09-22 09:06:36 +0200

    build: add $ORIGIN/../lib as RPATH
    When we build binaries in gitlab-ci, the binaries' RPATH is set to
    /opt/syslog-ng. When we download them with installdeps.py, they won't be
    able to find the dynamically linked libraries, because they are not in
    /opt/syslog-ng, but in @@installdir@@.
    
    We can build the binaries with explicitly set the RPATH to
    $ORIGIN/../lib.
    
    $ORIGIN is evaluated runtime to the binary's current directory,
    ../lib provides the correct lib directory to the binaries in the bin and
    sbin directories, too.
    
    See "Rpath token expansion" in:
    https://man7.org/linux/man-pages/man8/ld.so.8.html
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ ls /opt/syslog-ng/lib/
 
 export PKG_CONFIG_PATH="/opt/syslog-ng/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LIBRARY_PATH="$LIBRARY_PATH:/opt/syslog-ng/lib"
-export CFLAGS="-O2 -isystem /opt/syslog-ng/include -Wl,-R/opt/syslog-ng/lib"
+export CFLAGS='-O2 -isystem /opt/syslog-ng/include -Wl,-z,origin,-rpath,\$$ORIGIN/../lib'
 
 sh autogen.sh
 ./configure --prefix=/opt/syslog-ng/ --disable-static --enable-shared --disable-gtk-doc --with-libiconv=gnu --enable-debug
